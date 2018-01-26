cc4225b0f9b1468abadfd71e1fb56b6ae905070b
Alnajafi <asamin@netapp.com>
2018-01-26 11:04:34 +0000

    added authentication for booking

--- selab/app/Http/Controllers/BookingsController.php
+++ selab/app/Http/Controllers/BookingsController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class BookingsController extends Controller
 {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+	
 	public function index(){
 		$title = 'Book a slot';
 		return view('bookings.index')->with('title',$title);
--- selab/resources/views/bookings/index.blade.php
+++ selab/resources/views/bookings/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>{{$title}}</h1>
+	@if(!Auth::guest())
+		<h1>{{$title}}</h1>
+		<hr/>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="panel panel-default">
+						
+						<div class="panel-heading">Book a slot</div>
+						
+						<div class="panel-body">
+							<h3>Available slots</h3>
+							<table class="table table-striped">
+								<tr>
+									<th>First Name</th>
+									<th>Last Name</th>
+									<th>City</th>
+									<th></th>
+								</tr>
+							</table>
+							@endif
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 @endsection
