ffa150a136b7063621dce424a8137c9269f2fa9f
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-02-13 11:25:26 +0000

    Added functions to get a users booking date and time. Also changed relationship to calendar to hasMany

--- selab/app/Booking.php
+++ selab/app/Booking.php
@@ -1,21 +1,32 @@
 <?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-use App\User;
-
-class Booking extends Model
-{
-	protected $table = 'bookings'; // you may change this to your name table
-	public $timestamps = true; // set true if you are using created_at and updated_at
-	protected $primaryKey = 'id'; // the default is id
-
-	public function user(){
-		return $this->belongsTo('App\User', 'user_id');
-	}
 	
-	public function calendar(){
-		return $this->hasOne('App\Calendar', 'id');
+	namespace App;
+	
+	use Illuminate\Database\Eloquent\Model;
+	use App\User;
+	use App\Calendar;
+	
+	class Booking extends Model
+	{
+		protected $table = 'bookings'; // you may change this to your name table
+		public $timestamps = true; // set true if you are using created_at and updated_at
+		protected $primaryKey = 'id'; // the default is id
+		
+		public function user(){
+			return $this->belongsTo('App\User', 'user_id');
+		}
+		
+		public function calendar(){
+			return $this->hasMany('App\Calendar', 'id');
+		}
+		
+		public function calendarDate($id){
+			$calendar =  Calendar::find($id);
+			return $calendar->date;
+		}
+		
+		public function calendarTime($id){
+			$calendar =  Calendar::find($id);
+			return $calendar->time;
+		}
 	}
-}
