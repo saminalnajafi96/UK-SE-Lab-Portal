51ec8b0d77cea8fc0b5a5102709aea08133cd71e
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-02-13 11:27:42 +0000

    Returning bookings specific to user

--- selab/app/Http/Controllers/HomeController.php
+++ selab/app/Http/Controllers/HomeController.php
@@ -1,29 +1,37 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Environment;
-
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
-}
+	
+	namespace App\Http\Controllers;
+	
+	use Illuminate\Http\Request;
+	use App\Environment;
+	use App\Booking;
+	use App\User;
+	
+	class HomeController extends Controller
+	{
+		/**
+		 * Create a new controller instance.
+		 *
+		 * @return void
+		 */
+		public function __construct()
+		{
+			$this->middleware('auth');
+		}
+		
+		/**
+		 * Show the application dashboard.
+		 *
+		 * @return \Illuminate\Http\Response
+		 */
+		public function index()
+		{
+//			$bookings = Booking::orderBy('id','asc')
+//					->where('user_id', '=', auth()->user()->id)
+//					->get();
+			$user_id = auth()->user()->id;
+			$user = User::find($user_id);
+			
+			return view('home')->with('bookings',$user->bookings);
+		}
+	}
