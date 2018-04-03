6de79c725400b514d3c6ad991e58fb800ff165f6
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-04-03 14:46:16 +0100

    show contents of the environment with connect buttons to relative management tools

--- selab/resources/views/environments/show.blade.php
+++ selab/resources/views/environments/show.blade.php
@@ -3,8 +3,7 @@
 @section('content')
 	<a href="/environments" class="btn btn-default">Go back</a>
 	<h1>{{$environment->environment_name}}</h1>
-	<hr/>
-	<div>
+	<div class="row">
 		<h3>Description</h3> <hr/>
 		{{$environment->description}} <br/>
 		
@@ -43,42 +42,83 @@
 		</div>
 		<br/><br/>
 		
-		@foreach($hardware as $item)
-			<h5>{{$item->model}}</h5>
-			<div class="system">
-				<img src="/storage/images/{{$item->front_image}}" id="{{$item->name}}_Front" class="front">
-				<span class="zoom">
-					<img src="/storage/images/{{$item->back_image}}" id="{{$item->name}}_Back" class="back">
+		<div class="group-panel">
+			@if(count($hardware) > 0)
+				@foreach($hardware as $item)
+					<div class="col-md-4 system">
+						<div class="panel custom-panel">
+							<div class="panel-heading">
+								<h5>{{$item->model}}</h5>
+							</div>
+							<div>
+								<img src="/storage/images/{{$item->front_image}}" id="{{$item->model}}_N{{$item->node_number}}_Front" class="front">
+								<span class="zoom">
+					<img src="/storage/images/{{$item->back_image}}" id="{{$item->model}}_N{{$item->node_number}}_Back" class="back">
 				</span>
-				@if(!Auth::guest())
-					<div class="details front" id="{{$item->model}}_hover">
-						<ul>
-							<li>Node IP address: {{$item->node_ip}}</li>
-							<li>OS: {{$item->os}}</li>
-							@if($item->management_ip != null)
-								<li>Management IP: {{$item->management_ip}}</li>
-							@endif
-						</ul>
+							</div>
+							<div class="panel-body">
+								@if(Auth::guest())
+									<p>Please log in to see the details of this system</p>
+								@else
+									<ul>
+										<li>Node {{$item->node_number}}</li>
+										<li>Node IP address: {{$item->node_ip}}</li>
+										<li>OS: {{$item->os}}</li>
+									</ul>
+								@endif
+							</div>
+						</div>
 					</div>
-				@endif
-			</div>
-			<br/>
-		@endforeach
-		<hr/><br/>
-		
-		@if(!Auth::guest())
-			@foreach($hardware as $item)
-				@if($item->management_ip != null)
-					<a href="http://{{$item->management_ip}}" target="_blank">
-						<button class="btn btn-primary btn-sm" type="button">Connect to System Manager</button>
-					</a>
-				@else
-					<a href="http://{{$item->node_ip}}" target="_blank">
-						<button class="btn btn-primary btn-sm" type="button">Connect to {{$item->model}}</button>
-					</a>
-				@endif
-			
-			@endforeach
+				@endforeach
+			@else
+				There is no hardware assigned to this environment.
+			@endif
+		</div>
+	</div>
+	
+	<div class="row">
+		<hr/>
+		@if(Auth::guest())
+			Log in to be able to book a slot and connect to this environment <br/><br/>
+			@else
+			<strong>NOTE: Please do not connect to an environment if you have not booked a slot. You may be working at the same time as someone else and risk having work being affected</strong> <br/><br/>
 		@endif
+		@foreach($hardware as $item)
+			@if($item->button == 0)
+				@continue
+			@endif
+			
+			@if($item->management_ip != null)
+				@if(Auth::guest())
+					<button class="btn btn-primary btn-sm" disabled>Connect to System Manager</button>
+				@else
+					<a class="connect" href="http://{{$item->management_ip}}" target="_blank">
+						<button class="btn btn-primary btn-sm">Connect to System Manager</button>
+					</a>
+				@endif
+			@else
+				@if(Auth::guest())
+					<button class="btn btn-primary btn-sm" disabled>Connect to {{$item->model}}</button>
+				@else
+					<a class="connect" href="http://{{$item->node_ip}}" target="_blank">
+						<button class="btn btn-primary btn-sm">Connect to {{$item->model}}</button>
+					</a>
+				@endif
+			@endif
+		@endforeach
+		
+	
+		{{--<h4>Connect to environment</h4>--}}
+		{{--<div class="col-md-4">--}}
+			{{--<div class="form-group">--}}
+				{{--{!! Form::open(['action' => 'EnvironmentsController@checkPass','method' => 'POST']) !!}--}}
+				{{--<label class="control-label" for="password">Enter the password for your booking here</label>--}}
+				{{--<input class="form-control" name="password" type="text">--}}
+				{{--<button class="btn btn-primary btn-sm" style="margin-top: 10px" type="submit">Submit</button>--}}
+				{{--{!! Form::close() !!}--}}
+			{{--</div>--}}
+		{{--</div>--}}
+		
+		<br/><br/>
 	</div>
 @endsection
