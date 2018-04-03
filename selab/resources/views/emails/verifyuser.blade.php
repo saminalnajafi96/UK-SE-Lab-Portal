1ad23b6a375ea6855be6660e4934c3e9de346bc6
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-04-03 14:42:41 +0100

    playing around with the look of email template, no significant changes

--- selab/resources/views/emails/verifyuser.blade.php
+++ selab/resources/views/emails/verifyuser.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Please verify your email {{$user['name']}}</title>
-</head>
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--<head>--}}
+	{{--<title>Please verify your email {{$user['name']}}</title>--}}
+{{--</head>--}}
 
-<body>
-<h2>Thank you for registering to the UK SE Lab Portal, {{$user['name']}}</h2>
-<br/>
-Your registered email is: {{$user['email']}}. Please click on the below link to verify your email account: <br/>
-<a href="{{url('user/verify', $user->verifyUser->token)}}">Verify Email</a>
-<br/>
-Please do not reply to this email
-</body>
-</html>
+{{--<body>--}}
+{{--<h2>Thank you for registering to the UK SE Lab Portal, {{$user['name']}}</h2>--}}
+{{--<br/>--}}
+{{--Your registered email is: {{$user['email']}}. Please click on the below link to verify your email account: <br/>--}}
+{{--<a href="{{url('user/verify', $user->verifyUser->token)}}">Verify Email</a>--}}
+{{--<br/>--}}
+{{--Please do not reply to this email--}}
+{{--</body>--}}
+{{--</html>--}}
+
