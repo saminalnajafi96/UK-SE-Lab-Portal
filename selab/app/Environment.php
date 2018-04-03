4b1b43cf753b3739c3dcec9246ac052c9c50cc32
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-04-03 14:35:35 +0100

    Update. No change to model

--- selab/app/Environment.php
+++ selab/app/Environment.php
@@ -9,4 +9,8 @@ class Environment extends Model
 	protected $table = 'environments';
 	protected $primaryKey = 'id';
 	public $timestamps = false;
+	
+	public function hardware(){
+		return $this->hasMany('App\Hardware');
+	}
 }
