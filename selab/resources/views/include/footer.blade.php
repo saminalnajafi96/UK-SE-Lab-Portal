62ad123b065c8bbd3e483838e510290fce245f08
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-04-09 13:26:43 +0100

    Adding footer

---
+++ selab/resources/views/include/footer.blade.php
@@ -0,0 +1,46 @@
+<!--Footer-->
+<footer class="page-footer font-small pt-4 mt-4">
+	
+	<!--Footer Links-->
+	<div class="container-fluid text-center text-md-left">
+		<div class="row">
+			
+			<!--First column-->
+			<div class="col-md-6">
+				<h5 class="text-uppercase">Footer Content</h5>
+				<p>Here you can use rows and columns here to organize your footer content.</p>
+			</div>
+			<!--/.First column-->
+			
+			<!--Second column-->
+			<div class="col-md-6">
+				<h5 class="text-uppercase">Links</h5>
+				<ul class="list-unstyled">
+					<li>
+						<a href="#!">Link 1</a>
+					</li>
+					<li>
+						<a href="#!">Link 2</a>
+					</li>
+					<li>
+						<a href="#!">Link 3</a>
+					</li>
+					<li>
+						<a href="#!">Link 4</a>
+					</li>
+				</ul>
+			</div>
+			<!--/.Second column-->
+		</div>
+	</div>
+	<!--/.Footer Links-->
+	
+	<!--Copyright-->
+	<div class="footer-copyright py-3 text-center">
+		© 2018 Copyright:
+		<a href="https://mdbootstrap.com/material-design-for-bootstrap/"> MDBootstrap.com </a>
+	</div>
+	<!--/.Copyright-->
+
+</footer>
+<!--/.Footer-->
